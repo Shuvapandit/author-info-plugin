9189cc3fea629eeef908f1fce8ba83e6c1375381
Shuvapandit <chakrabortyshuva216@gmail.com>
2023-02-19 00:32:50 +0600

    Create shortcode to display author info

--- author-info-plugin.php
+++ author-info-plugin.php
@@ -25,4 +25,19 @@ function add_author_info_after_post() {
 }
 add_action('wp_footer', 'add_author_info_after_post');
 
-
+// Create shortcode to display author info
+function author_info_shortcode() {
+    $author_id = get_the_author_meta('ID');
+    $author_name = get_the_author_meta('display_name');
+    $author_avatar = get_avatar_url($author_id);
+    $author_website = get_the_author_meta('user_url');
+    ob_start(); ?>
+    <div class="author-info">
+        <h3><?php echo esc_html($author_name); ?></h3>
+        <img src="<?php echo esc_url($author_avatar); ?>" alt="<?php echo esc_attr($author_name); ?>" />
+        <p><?php echo esc_html($author_website); ?></p>
+    </div>
+    <?php
+    return ob_get_clean();
+}
+add_shortcode('author_info', 'author_info_shortcode');
