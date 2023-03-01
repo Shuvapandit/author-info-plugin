aa00b8610a92b17856e9a1773b01b2c411307d60
Shuvapandit <chakrabortyshuva216@gmail.com>
2023-03-01 13:27:44 +0600

    Generate HTML for author details s which will take the author object or id as input, and output the html. Then call this function from both the hook func and the shortcode commit

--- author-info-plugin.php
+++ author-info-plugin.php
@@ -12,41 +12,68 @@
  * Update URI:       https://github.com/Shuvapandit
 */
 
-//Add Css
+//add Css
 function add_author_info_plugin_styles() {
-    wp_enqueue_style( 'author-info-plugin-styles', plugins_url( 'css/sstt-style.css', __FILE__ ) );
+    wp_enqueue_style( 'author-info-plugin-styles', plugins_url( 'css/style.css', __FILE__ ) );
 }
 add_action( 'wp_enqueue_scripts', 'add_author_info_plugin_styles' );
 
-// Add author info after post content
-add_filter( 'the_content', 'add_author_info_after_post' );
 
+/*
+
+ * Generate HTML for author details.
+
+ */
+function generate_author_details_html( $author ) {
+    $author_id = is_object( $author ) ? $author->ID : (int) $author;
+    $author_data = get_userdata( $author_id );
+
+    if ( ! $author_data ) {
+        return '';
+    }
+
+    $output = '<div class="author-details">';
+    $output .= '<div class="author-avatar author-avatar-img">' . get_avatar( $author_id ) . '</div>';
+    $output .= '<div class="author-name">' . esc_html( $author_data->display_name ) . '</div>';
+    $output .= '<div class="author-website"><a href="' . esc_url( $author_data->user_url ) . '">' . esc_html( $author_data->user_url ) . '</a></div>';
+    $output .= '</div>';
+
+    return $output;
+}
+
+/*
+ * Add author details after post content.
+ 
+ */
 function add_author_info_after_post( $content ) {
     if ( is_singular( 'post' ) ) {
-        $author_info = author_info_shortcode();
-        $content .= '<div class="author-info">' . $author_info . '</div>';
+        global $post;
+        $author = get_user_by( 'ID', $post->post_author );
+        $author_details_html = generate_author_details_html( $author );
+        $content .= $author_details_html;
     }
+
     return $content;
 }
 
-// Shortcode for author info
+add_filter( 'the_content', 'add_author_info_after_post' );
+
+
+
+/*  
+* Author info shortcode. 
+*/
+ 
+function author_info_shortcode( $atts ) {
+    $atts = shortcode_atts( array(
+        'id' => get_the_author_meta( 'ID' ),
+    ), $atts, 'author_info' );
+
+    $author = get_user_by( 'ID', $atts['id'] );
+    return generate_author_details_html( $author );
+}
+
 add_shortcode( 'author_info', 'author_info_shortcode' );
 
-function author_info_shortcode() {
-    $author_id = get_the_author_meta( 'ID' );
-    $author_name = get_the_author_meta( 'display_name' );
-    $author_avatar = get_avatar( $author_id, 96 );
-    $author_website = get_the_author_meta( 'user_url' );
-    
-    ob_start();
-    ?>
-    <div class="author-info-shortcode">
-        <p><strong>Name:</strong> <?php echo $author_name; ?></p>
-        <p><strong>Avatar:</strong> <?php echo $author_avatar; ?></p>
-        <?php if ( $author_website ) { ?>
-            <p><strong>Website:</strong> <a href="<?php echo esc_url( $author_website ); ?>"><?php echo esc_url( $author_website ); ?></a></p>
-        <?php } ?>
-    </div>
-    <?php
-    return ob_get_clean();
-}
+   
+?>
